0b291a7890964c9017405630a00b082d8925d444
Glitch (glitch-blank-website) <none>
2022-09-23 16:33:15 +0000

    ✌️🛀 Checkpoint ./script.js:29322470/2104 ./package.json:29322470/4488 ./server.js:29322470/1311

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+/*
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});*/
+
+var express = require('express'),
+    app = express();
+app.use(express.static(__dirname + '/public'));
+app.listen(port);
